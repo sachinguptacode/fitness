645e522887ee6482734d888b4b561c2f3f61f494
sachinguptacode <sachingupta705242@gmail.com>
2025-07-28 22:38:28 +0530

    .env file chenge

--- back-end/index.js
+++ back-end/index.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 const app = exp();
 dotenv.config();
 app.use(exp.json());
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use("/api/v1", router);
 dbConnect();
 
